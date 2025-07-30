74c2ed268d575bfb1aa949c9ee31cd1cb7f23c83
goldengamer14 <maazubayr1234@gmail.com>
2025-07-30 16:09:24 +0530

    Modified files.bat and renamed initialize_cmd.bat with 00

--- cmd_practice/00_initialize_cmd.bat
+++ cmd_practice/00_initialize_cmd.bat
@@ -1,11 +1,11 @@
 @echo off
 tasklist /fi "IMAGENAME eq cmd.exe" | find /i "cmd.exe" > nul
+cls
 if errorlevel 1 (
     echo CMD is not running.
-    start cmd.exe /k
+    start cmd.exe /c
 ) else (
     echo CMD is already running.
 )
 if exist cmd_practice\ cd cmd_practice
-cls
 cmd
--- cmd_practice/files.bat
+++ cmd_practice/files.bat
@@ -1,37 +1,28 @@
 @echo off
-setlocal
 cls
-@REM Initialize or empty log files
-if exist deletion_log.log del /f /q deletion_log.log
-if exist log_robocopy.log del /f /q log_robocopy.log
-if exist my_git_repo_struct.txt del /f /q my_git_repo_struct.txt
-
-echo Listing files in cmd_practice directory:
-echo
-(
-    dir /b
+cmd /c
+echo.
+echo.
+setlocal
+echo Initializing log files...
+setlocal enabledelayedexpansion
+for %%f in (deletion_log.log log_robocopy.log my_git_repo_struct.txt) do (
+    type nul > %%f
+    echo Content %type %%f%
 )
 echo.
-
-echo Initializing log files...
-
-type nul > deletion_log.log
-type nul > log_robocopy.log
-type nul > my_git_repo_struct.txt
+endlocal
 
 echo Log files initialized.
 echo.
 
 echo Listing files in cmd_practice directory:
-echo
-(
-    dir /b
-)
+dir /b
 echo.
 
 @REM used >> instead of > to append logs
 if exist ..\Maaz.js_copy (
-    del /f /q /s ..\Maaz.js_copy\* > deletion_log.log
+    del /f /q /s ..\Maaz.js_copy\* >> deletion_log.log
     echo Files in Maaz.js_copy deleted.
 ) else (
     md ..\Maaz.js_copy
@@ -39,7 +30,7 @@ if exist ..\Maaz.js_copy (
 )
 echo.
 
-robocopy /s ..\Maaz.js\ ..\Maaz.js_copy\ > log_robocopy.log
+robocopy /s ..\Maaz.js\ ..\Maaz.js_copy\ >> log_robocopy.log
 if %errorlevel% neq 0 (
     echo Error occurred during file copy. Check log_robocopy.txt for details.
 ) else (
@@ -52,7 +43,7 @@ del /f /s /q ..\Maaz.js_copy\* >> deletion_log.log
 rd /s /q ..\Maaz.js_copy
 echo Maaz.js_copy cleaned up.
 
-tree /f ..\ > my_git_repo_struct.txt
+tree /f ..\ >> my_git_repo_struct.txt
 if %errorlevel% neq 0 (
     echo Error occurred while generating directory structure.
 ) else (
@@ -63,3 +54,5 @@ echo.
 set "currentdir=%cd%"
 echo You are currently working in ^<%currentdir%^>
 endlocal
+echo.
+echo.
